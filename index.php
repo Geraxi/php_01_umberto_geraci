799c48ad9c9595e1e568b54bf2d3b48550ae1dcf
Geraxi <umbertogeraci0@gmail.com>
2025-02-04 20:54:03 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+const INTERO= 27;
+const VIRGOLA= 3.5;
+const STRINGA= "stringa";
+const BOOLEANO= true;
+
+
+var_dump(INTERO);
+var_dump(VIRGOLA);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
